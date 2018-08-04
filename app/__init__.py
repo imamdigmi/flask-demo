b446a54dccc2ee7dad89f4b0d7a1ce8618a96f76
imamdigmi <imamdigmi@gmail.com>
2018-08-04 16:47:26 +0700

    Add sending email and reset password

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 
@@ -13,6 +14,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+mail = Mail(app)
 login.login_view = 'login'
 
 if not app.debug:
--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
-from app import db, login
+import jwt
+from app import app, db, login
+from time import time
 from hashlib import md5
 from datetime import datetime
 from flask_login import UserMixin
@@ -67,6 +69,22 @@ class User(UserMixin, db.Model):
                 followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
+
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
 
 
 class Post(db.Model):
