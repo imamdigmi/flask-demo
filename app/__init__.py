71a5f52ce159f766017e09284dc827b0c7356fc9
imamdigmi <imamdigmi@gmail.com>
2018-08-04 18:54:59 +0700

    Add bootstrap css

--- app/__init__.py
+++ app/__init__.py
@@ -6,15 +6,19 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
+
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 mail = Mail(app)
+bootstrap = Bootstrap(app)
+
 login.login_view = 'login'
 
 if not app.debug:
