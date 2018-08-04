ab4134cc7abe8004988779c55a293f2b633f5d49
imamdigmi <imamdigmi@gmail.com>
2018-08-05 01:29:01 +0700

    Add blueprint stuctures

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
+    FLASK_DEBUG = os.environ.get('DEBUG')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -11,7 +16,7 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = [os.environ.get('MAIL_ADDRESS')]
 
     POSTS_PER_PAGE = 15
     LANGUAGES = ['en', 'es', 'fr', 'id', 'ru', 'ja']
