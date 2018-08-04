5edd8df1ae7596bdb244d2b281967a5ec79cf29f
imamdigmi <imamdigmi@gmail.com>
2018-08-04 19:31:04 +0700

    Handling datetime with Moment.js

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 
@@ -18,6 +19,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 login.login_view = 'login'
 
